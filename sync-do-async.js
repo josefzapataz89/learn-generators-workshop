fc984bee791b171bed0dbc9b28ca2382db53c5dd
Jose Fernando Zapata Zambrano <joferzz1989@gmail.com>
2019-11-08 10:13:59 -0400

    :jack_o_lantern: feat: finish look sync do async

--- sync-do-async.js
+++ sync-do-async.js
@@ -3,13 +3,28 @@
 var fs = require('fs');
 
 function run(generator) {
-    // improve `run` above
+    var it = generator(go);
+    
+    function go (err, result) {
+        if (err)
+            return it.throw(err);
+
+        it.next(result);
+    }
+
+    go();
 }
 
 run(function* (done) {
     // catch exception
-    var dirFiles = yield fs.readdir('NoNoNoNo', done); // No such dir
-    var firstFile = dirFiles[0]; // TypeError: Cannot read property '0' of undefined
+    var firstFile;
 
+    try {
+        var dirFiles = yield fs.readdir('NoNoNoNo', done); // No such dir
+        firstFile = dirFiles[0]; // TypeError: Cannot read property '0' of undefined
+    } catch (error) {
+        firstFile = null;
+    }
+    
     console.log(firstFile);
 });
